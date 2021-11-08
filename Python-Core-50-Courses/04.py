28a86e6beff18f00a27d380dbf546e761c43b3a0
charliehsu <charlie_hsu@o.winho.com.tw>
2021-11-08 17:49:30 +0800

    Lession04

--- Python-Core-50-Courses/04.py
+++ Python-Core-50-Courses/04.py
@@ -29,9 +29,29 @@
 
 # 運算符的例子
 # 例子1：华氏温度转换为摄氏温度 C = (F - 32) / 1.8
-f = float(input('請輸入華氏溫度： '))
-c = (f - 32) / 1.8
-print('%.1f華氏溫度 = %.1f攝氏溫度' % (f, c))
-# 格式化字符串f-string 
-# 參考連結：https://blog.csdn.net/sunxb10/article/details/81036693
-print(f'{f:.1f}華氏溫度 = {c:.1f}攝氏溫度')
+# f = float(input('請輸入華氏溫度： '))
+# c = (f - 32) / 1.8
+# print('%.1f華氏溫度 = %.1f攝氏溫度' % (f, c))
+# # 格式化字符串f-string 
+# # 參考連結：https://blog.csdn.net/sunxb10/article/details/81036693
+# print(f'{f:.1f}華氏溫度 = {c:.1f}攝氏溫度')
+
+# 例子2：输入圆的半径计算计算周长和面积
+# radius = float(input('請輸入半徑： '))
+# perimeter = 2 * radius * 3.14
+# area = radius * radius * 3.14
+# print(f'周長：{perimeter:.4f} , 面積：{area:.4f}')
+
+# 例子3：输入年份判断是不是闰年
+year = int(input('請輸入年份：'))
+# python沒有&&要寫and 
+is_lunar = ((year % 4 == 0) and (year % 100 != 0) and (year % 400 == 0))
+# python沒有三元運算子，以下是if的寫法
+# 寫法1
+# if is_lunar:
+#     print(f'{year}年是閏年')
+# else:
+#     print(f'{year}年不是閏年')
+# 寫法2
+# 成立的狀況 if 布林 else 不成立的狀況
+print(f'{year}年是閏年') if is_lunar else print(f'{year}年不是閏年')
--- Python-Core-50-Courses/README.md
+++ Python-Core-50-Courses/README.md
@@ -1,6 +1,8 @@
-文章鏈結
-https://github.com/jackfrued/Python-Core-50-Courses
+# README #
+[教學鏈結](https://github.com/jackfrued/Python-Core-50-Courses)
 
-Lession 02. print 'hello world'
-Lession 03. 變數命名&類型
-Lession 04. 運算符
+### 目錄 ###
+
+* Lession 02. print 'hello world'
+* Lession 03. 變數命名&類型
+* Lession 04. 運算符
