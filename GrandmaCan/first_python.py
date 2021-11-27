569d699b74d9d6780d84c6a9e7f088a64e770f92
charliehsu <stu610042002@yahoo.com.tw>
2021-11-27 23:04:22 +0800

    [自學]測試上傳身分

--- GrandmaCan/first_python.py
+++ GrandmaCan/first_python.py
@@ -14,4 +14,4 @@
 # '字串'[n]:取字串中的第幾個字(從0開始算)(超過會跳錯誤IndexError: string index out of range)
 # '字串'.index('搜尋字串'):找搜尋字串在原字串中"首次出現"的位置(return integer)(找不到會跳錯誤ValueError: substring not found)
 # '字串'.replace('被替換字串', '替換成什麼字串'):取代文字(找到幾組就取代幾組)
-print('哈哈es'.replace('蛤', 'c'))
+# print('哈哈es'.replace('蛤', 'c'))
