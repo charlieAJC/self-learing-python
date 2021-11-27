625a90af62e81b7d6a63c2e495fccfb819774d69
charliehsu <stu610042002@yahoo.com.tw>
2021-11-27 19:41:10 +0800

    [自學]YT影片自學

---
+++ first_python.py
@@ -0,0 +1,19 @@
+# https://www.youtube.com/watch?v=zdMUJJKFdsU
+# 下次從 number 開始
+# https://www.youtube.com/watch?v=2scOhFObj-4 7個小時的課程wtf
+# 打印
+# print('consolas')
+
+# 變數宣告不須指定型態 也沒有符號前綴
+# 變數名稱只允許英文/數字/_的組合 開頭不得為數字
+
+# 合併字串用 +
+# 字串中換行用 \n
+# 字串中的跳脫字元為 \
+# '字串'.lower():字串英文字母全變小寫
+# '字串'.upper():字串英文字母全變大寫
+# '字串'.isupper():檢查字串中的英文字母是否全為大寫(return boolean)
+# '字串'[n]:取字串中的第幾個字(從0開始算)(超過會跳錯誤IndexError: string index out of range)
+# '字串'.index('搜尋字串'):找搜尋字串在原字串中"首次出現"的位置(return integer)(找不到會跳錯誤ValueError: substring not found)
+# '字串'.replace('被替換字串', '替換成什麼字串'):取代文字(找到幾組就取代幾組)
+print('哈哈es'.replace('蛤', 'c'))
